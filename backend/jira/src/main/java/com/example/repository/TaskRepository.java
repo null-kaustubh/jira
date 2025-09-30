249e7d7523210c81dd6c7b478236b3e906097e9d
AaronPereira21 <aaronpereira345@gmail.com>
2025-09-30 15:46:34 +0530

    updated the task sservice controller and  repo

--- backend/jira/src/main/java/com/example/repository/TaskRepository.java
+++ backend/jira/src/main/java/com/example/repository/TaskRepository.java
@@ -2,9 +2,11 @@ package com.example.repository;
 
 import com.example.model.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
-    		
+    List<Task> findByProjectProjectId(Long projectId);
 }
