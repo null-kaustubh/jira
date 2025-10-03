b8308d3e846ab69f13dff9782ec404678becb058
Aditya Gaikwad <Aditya.G@accelya.com>
2025-10-03 13:40:09 +0530

    add task utils

---
+++ backend/jira/src/main/java/com/example/service/TaskUtils.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.example.model.entity.Task;
+
+@Component
+public class TaskUtils {
+
+	private final List<String> STATUS_FLOW = List.of("TODO", "IN_PROGRESS", "IN_REVIEW", "DONE");
+
+	public int getStatusIndex(String status) {
+		return STATUS_FLOW.indexOf(status);
+	}
+
+    public boolean isStepSkipAllowed(String oldStatus, String newStatus, boolean isManager){
+        int oldIndex = STATUS_FLOW.indexOf(oldStatus);
+        int newIndex = STATUS_FLOW.indexOf(newStatus);
+
+        if(isManager && newIndex==0 && oldIndex==2) return true;
+        return Math.abs(newIndex - oldIndex) != 1;
+    }
+
+    public void validateUpdatedTaskStatus(String oldStatus, String newStatus) {
+        if (isStepSkipAllowed(oldStatus, newStatus, false)) throw new IllegalArgumentException("Invalid status transition. Cannot skip steps.");
+        
+        if (STATUS_FLOW.indexOf(newStatus) < STATUS_FLOW.indexOf(oldStatus)) throw new IllegalArgumentException("Employees cannot move status backward.");
+
+        if ("IN_REVIEW".equals(oldStatus) && "DONE".equals(newStatus)) throw new IllegalArgumentException("Only admin/manager can mark IN_REVIEW as DONE.");
+	}
+
+}
